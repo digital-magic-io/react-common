904f56d405755e5ee8933db733b84734fa4bb35b
jgoloborodko <jevgeni@digital-magic.io>
2020-10-02 17:25:19 +0300

    Improved auth context: added builder

---
+++ src/context/auth-context.ts
@@ -0,0 +1,20 @@
+import React from 'react'
+import { isNotEmptyString } from '@digital-magic/ts-common-utils/lib/type'
+
+export type AuthContextType = {
+  readonly authorized: boolean
+}
+
+export type AuthContext<T extends AuthContextType> = React.Context<T>
+
+export type AuthContextProps = {
+  readonly children: React.ReactNode
+}
+
+export const createAuthContext = <T extends AuthContextType>(defaultValue: T, displayName?: string): AuthContext<T> => {
+  const ctx: AuthContext<T> = React.createContext<T>(defaultValue)
+  if (isNotEmptyString(displayName)) {
+    ctx.displayName = displayName
+  }
+  return ctx
+}
